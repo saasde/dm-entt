e52b6648f409af405ea989c38908ab2df7b18bbf
saasde <76675598+saasde@users.noreply.github.com>
2021-01-21 11:32:39 +0900

    V2

--- js/header.js
+++ js/header.js
@@ -7,7 +7,6 @@ $(function(){
        $("#header .gnb li a").not(this).css({'opacity':'1', 'transition':'all 0.5s'});
    });
     
-    
    $(".sns li a").eq(0).click(function(e){
        e.preventDefault();
       $("body").toggleClass('bgon').css({'transition':'all 0.3s'});
@@ -32,16 +31,15 @@ $(function(){
    $(window).scroll(function() {
         if ($(this).scrollTop() > 300) {
             $('.top').fadeIn(500);
-            $('.gnb').fadeOut();
             $('.sns li').not('.top').fadeOut();
             $('.sns hr').fadeOut();
         } else {
             $('.top').fadeOut(500);
-            $('.gnb').fadeIn();
             $('.sns li').not('.top').fadeIn();
             $('.sns hr').fadeIn();
         }
     });
+
     $(".top").click(function(e) {
         e.preventDefault();
         $('html, body').animate({
@@ -49,4 +47,49 @@ $(function(){
         }, 300);
             return false;
     });
+    
+   /* 햄버거 버튼 메뉴 */
+            var num = 0;
+            $(".ham").click(function(e){
+                e.preventDefault();
+                if(num==0){
+                    $(".ham>a>img").attr('src', '../images/close.png');
+//                    $("#header").css({'position': 'fixed', 'width':'100%', 'height':'100%', 'background':'blue', 'transition':'all 0.5s', 'z-index':'999'});
+                    $("#gnbon").fadeIn(500);
+                    $('body').css({'opacity':'0.3'});
+                    
+                    num=1;
+                } else {
+                    $(".ham>a>img").attr('src', '../images/menu.png');
+//                    $("#header").css({'position': 'fixed', 'width':'100%', 'height':'182px', 'background':'none', 'transition':'all 0.5s', 'z-index':'999'});
+                    $("#gnbon").fadeOut(100);
+                    $('body').css({'opacity':'1'});
+                    
+                    num=0;
+                }
+            });
+            
+//            var number = 0;
+//            $("#gnbon>li>a>span>i").click(function(e){
+//               e.preventDefault();
+//                if(number==0){
+//                    $(this).css({'transform':'rotate(180deg)', 'transition':'all 0.3s'});
+//                    number = 1;
+//                } else {
+//                    $(this).css({'transform':'rotate(0deg)', 'transition':'all 0.3s'});
+//                    number = 0;
+//                }
+//            });
+    
+    
+            $(".down2").eq(0).click(function(e){
+                e.preventDefault();
+               $(".subon").eq(0).addClass(".on").slideToggle('.on'); 
+                $(this).toggleClass('down2on');
+            });
+            $(".down2").eq(1).click(function(e){
+                e.preventDefault();
+               $(".subon").eq(1).addClass(".on").slideToggle('.on'); 
+                $(this).toggleClass('down2on');
+            });
 });
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-$(function(){
+$(function(){    
+    
    $("#header .gnb li a").mouseenter(function(){
        $(this).css({'transform':'translate(5px,0)', 'transition':'all 0.5s'});
        $("#header .gnb li a").not(this).css({'opacity':'0.5', 'transition':'all 0.5s'});
@@ -31,12 +32,10 @@ $(function(){
    $(window).scroll(function() {
         if ($(this).scrollTop() > 300) {
             $('.top').fadeIn(500);
-            $('.gnb').fadeOut();
             $('.sns li').not('.top').fadeOut();
             $('.sns hr').fadeOut();
         } else {
             $('.top').fadeOut(500);
-            $('.gnb').fadeIn();
             $('.sns li').not('.top').fadeIn();
             $('.sns hr').fadeIn();
         }
@@ -49,6 +48,51 @@ $(function(){
         }, 300);
             return false;
     });
+    
+   /* 햄버거 버튼 메뉴 */
+            var num = 0;
+            $(".ham").click(function(e){
+                e.preventDefault();
+                if(num==0){
+                    $(".ham>a>img").attr('src', 'images/close.png');
+//                    $("#header").css({'position': 'fixed', 'width':'100%', 'height':'100%', 'background':'blue', 'transition':'all 0.5s', 'z-index':'999'});
+                    $("#gnbon").fadeIn(500);
+                    $('#wrap').css({'opacity':'0.3'});
+                    
+                    num=1;
+                } else {
+                    $(".ham>a>img").attr('src', 'images/menu.png');
+//                    $("#header").css({'position': 'fixed', 'width':'100%', 'height':'182px', 'background':'none', 'transition':'all 0.5s', 'z-index':'999'});
+                    $("#gnbon").fadeOut(100);
+                    $('#wrap').css({'opacity':'1'});
+                    
+                    num=0;
+                }
+            });
+            
+//            var number = 0;
+//            $("#gnbon>li>a>span>i").click(function(e){
+//               e.preventDefault();
+//                if(number==0){
+//                    $(this).css({'transform':'rotate(180deg)', 'transition':'all 0.3s'});
+//                    number = 1;
+//                } else {
+//                    $(this).css({'transform':'rotate(0deg)', 'transition':'all 0.3s'});
+//                    number = 0;
+//                }
+//            });
+    
+    
+            $(".down2").eq(0).click(function(e){
+                e.preventDefault();
+               $(".subon").eq(0).addClass(".on").slideToggle('.on'); 
+                $(this).toggleClass('down2on');
+            });
+            $(".down2").eq(1).click(function(e){
+                e.preventDefault();
+               $(".subon").eq(1).addClass(".on").slideToggle('.on'); 
+                $(this).toggleClass('down2on');
+            });
     
     
    var current = 0;
